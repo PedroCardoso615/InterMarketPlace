dedf63311714d9b8229dc7975569b22dc54f6855
Pedro Cardoso <pedroflfc@gmail.com>
2024-11-07 18:52:48 +0000

    App.js re ajusted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-
 import Home from './components/Home'
 import Catalog from './components/Catalog';
 import {Listing} from './components/Listing';
-import Profile from './components/Profile';
-import {Signup} from './components/Signup';
 import {Login} from './components/Login';
 
 function App() {
@@ -17,8 +14,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/listing" element={<Listing />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
